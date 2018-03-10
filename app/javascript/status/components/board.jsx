f37711543c4efe1e2dea1837be25ec37cc4b0818
GPif <mailgpif@gmail.com>
2018-03-10 16:24:13 +0100

    Load list of status on page load

--- app/javascript/status/components/board.jsx
+++ app/javascript/status/components/board.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import axios from 'axios';
 
-const Board = (props) => (
-  <div>Board</div>
-)
+class Board extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            status: []
+        };
+    }
+
+    fetchStatus() {
+        axios.get( '/candidat_status.json' )
+        .then(response => {
+            console.log(response);
+            this.setState({status: response.data.candidat_status});
+        })
+        .catch(error => {
+        });
+    }
+
+    componentDidMount () {
+        this.fetchStatus();
+    }
+
+    render () {
+        const li = this.state.status.map((x)=> { return <li key={x.name} > {x.name} {x.firstname} {x.status} </li> })
+        return <div>
+            <ul>{li}</ul>
+        </div>;
+    }
+}
 
 export default Board
